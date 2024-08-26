8ee85b01b0c64e09767722a50a8bffd479b6e925
JamesK <james.kavazy@gmail.com>
2024-08-26 11:30:19 +0100

    Handle empty form values and break app into components

--- src/app/payout-calculator/page.jsx
+++ src/app/payout-calculator/page.jsx
@@ -1,5 +1,8 @@
 'use client'
 import { useState } from 'react'
+import { PlayerForm } from "./PlayerForm"
+import { Results } from "./Results"
+
 import './payout.css'
 
 export default function Payout(){
@@ -7,7 +10,6 @@ export default function Payout(){
     const [newRow, setNewRow] = useState([
         { id: id, name: '', buyIn: '', cashOut: ''}]
     );
-
 
     const [results, setResults] = useState(false);
 
@@ -38,8 +40,13 @@ export default function Payout(){
     
 
     function submitForm() {
-        if (newRow.length === 0) {
-           setResults(false);
+        for (const row of newRow){
+            if (row.name === '' || row.buyIn === '' || row.cashOut === '') {
+                setResults(false);
+                console.log('Empty Row. Result State is now' + results);
+                return;
+            }
+
         }
        setResults(true);
        
@@ -61,100 +68,15 @@ export default function Payout(){
             {results ? (
                 <Results newRow={newRow} onBackPressed={onBackPressed}></Results>
                 ) : (
-                     <PlayerForm 
+                     <PlayerForm
                         submitForm={submitForm} 
                         newRow={newRow} 
                         handleInputChange={handleInputChange}
                         onAddPlayerBtnClick={onAddPlayerBtnClick}
                         onRemovePlayerBtnClick={onRemovePlayerBtnClick}    
-                   /> 
+                        /> 
                      )}
         </div>     
     );    
 }
-
-function FormRow({ id, name, buyIn, cashOut, onInputChange, onRemovePlayerBtnClick}){
-    return (
-        <div> 
-            <input 
-                id={id} 
-                className="name-input" 
-                type="text" 
-                autoFocus 
-                placeholder={"Player Name"} 
-                value={name} 
-                onChange={e => onInputChange(id, 'name', e.target.value)}
-            />
-                
-            <input 
-                className="buy-in-input" 
-                type="number" 
-                placeholder="Buy In" 
-                value={buyIn}
-                onChange={(e => onInputChange(id, 'buyIn', e.target.value))}
-            
-            />
-            <input 
-            className="cash-out-input" 
-            type="number" 
-            placeholder="Cash Out"
-            value={cashOut}
-            onChange={(e) => onInputChange(id, 'cashOut', e.target.value)}
-            />
-            <button onClick={() => onRemovePlayerBtnClick(id)}>Remove</button>
-            
-            <br></br><br></br>
-        </div>
-    );
-}
-
-
-function Results({ newRow, onBackPressed }){
-    return (
-        <>
-            <ul className='list'>
-                {newRow.map(row => {
-                    return (
-                        <li key={newRow.id}>
-                            {`${row.name} : £${Number(row.cashOut - row.buyIn)}`}
-                        </li>
-                    ) 
-                })} 
-            </ul>
-            <button onClick={onBackPressed}>Back</button>
-        </>
-
-    )
-}
-
-
-
-function PlayerForm({ submitForm, handleInputChange, newRow, onAddPlayerBtnClick, onRemovePlayerBtnClick }){
-    return (
-        
-            <div className="payout-container">
-                <div className="form-row">
-                    <form action={submitForm} className="new-player-form">
-                        {newRow.map((row) => (
-                            <FormRow
-                                key={row.id}
-                                id={row.id}
-                                name={row.name}
-                                buyIn={row.buyIn}
-                                cashOut={row.cashOut}
-                                onInputChange={handleInputChange} 
-                                onRemovePlayerBtnClick={onRemovePlayerBtnClick}
-                            />
-
-                        ))}  
-                        <button type='button' onClick={onAddPlayerBtnClick}>Add Player</button> 
-                        <br></br><br></br>
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>            
-            </div>
-            
-    );
-}
-
 
